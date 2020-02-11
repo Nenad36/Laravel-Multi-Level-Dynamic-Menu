8ce4e07852c43f6571705b2f1d0693a3ad2893a0
Nenad Pesic <nenad.pesic32@gmail.com>
2020-02-11 12:39:47 +0100

    Multi Level Dynamic Menu In Laravel Treeview

---
+++ app/Http/Controllers/MenuController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Menu;
+use Illuminate\Http\Request;
+
+class MenuController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $menus = Menu::where('parent_id', '=', 0)->get();
+        $allMenus = Menu::pluck('title','id')->all();
+        return view('menu.menuTreeview',compact('menus','allMenus'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required',
+        ]);
+        $input = $request->all();
+        $input['parent_id'] = empty($input['parent_id']) ? 0 : $input['parent_id'];
+        Menu::create($input);
+        return back()->with('success', 'Menu added successfully.');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show()
+    {
+        $menus = Menu::where('parent_id', '=', 0)->get();
+        return view('menu.dynamicMenu',compact('menus'));
+    }
+
+}
